95c98d836c44d2877b0c54b23ecc290e51ee4061
sangjunpark <juntech0421@github.com>
2020-09-08 18:02:47 +0900

    redux modules, store practice

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## 🔎Redux상태관리에 대한 연습을 위한 공간입니다.
+## 🔎 Redux상태관리에 대한 연습을 위한 공간입니다.
+
+- store store의 구독 상태관리<br>
+- react-redux의 Provider
+
+# 📌modules
+duck pattern module style
+action 선언 시 다른 모듈과 구분을 위해 접두사 파일이름을 삽입.
+app에서 불러온뒤 App 컴포넌트에 provider를 랩핑한 후 store값 삽입.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./exercise";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from './modules';
+
+
+const store = createStore(rootReducer);
+console.log(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
